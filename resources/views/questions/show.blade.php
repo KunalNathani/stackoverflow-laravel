5770a6c3a4ae9a7cebb1e1231813fd9ca3563963
Kunal Nathani <kunalnathani2042@gmail.com>
2022-08-28 11:55:05 +0530

    refactor: folder structure; seperate file to show answers

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -36,52 +36,6 @@
                 </div>
             </div>
         </div>
-        <div class="row mt-4">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="mt-0">{{ \Illuminate\Support\Str::plural('Answer', $question->answers_count) }}</h3>
-                    </div>
-                    <div class="card-body">
-                        @foreach($question->answers as $answer)
-                            {!! $answer->body !!}
-                            <div class="d-flex justify-content-between mr-3 mb-3">
-                                <div class="d-flex">
-                                    <div>
-                                        <a href="" title="Vote Up" class="vote-up d-block text-center text-dark">
-                                            <i class="fa fa-caret-up fa-3x"></i>
-                                        </a>
-                                        <h4 class="m-0 text-muted">45</h4>
-                                        <a href="" title="Vote Down" class="vote-down d-block text-center text-dark">
-                                            <i class="fa fa-caret-down fa-3x"></i>
-                                        </a>
-                                    </div>
-                                    <div class="mt-3 ms-3">
-                                        <a href="" title="Mark as Fav" class="favorite d-block text-center mb-2">
-                                            <i class="fa fa-star fa-2x text-dark"></i>
-                                        </a>
-                                        <h4 class="text-muted">123</h4>
-                                    </div>
-                                </div>
-                                <div class="d-flex flex-column">
-                                    <div class="text-end">
-                                        Answered {{ $answer->created_date }}
-                                    </div>
-                                    <div class="d-flex mt-2">
-                                        <div>
-                                            <img src="{{ $answer->author->avatar }}">
-                                        </div>
-                                        <div class="mt-2 ms-2">
-                                            {{ $answer->author->name }}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr>
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('answers._index')
     </div>
 @endsection
