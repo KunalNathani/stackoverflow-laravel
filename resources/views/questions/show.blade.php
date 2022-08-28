bda01f60cbec56397f4cce56ee699ca974faf033
Kunal Nathani <kunalnathani2042@gmail.com>
2022-08-28 11:49:07 +0530

    added buttons for up vote and down vote

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -1,5 +1,9 @@
 @extends('frontend_layout.app')
 
+@section('page-level-styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row">
@@ -42,7 +46,23 @@
                         @foreach($question->answers as $answer)
                             {!! $answer->body !!}
                             <div class="d-flex justify-content-between mr-3 mb-3">
-                                <div></div>
+                                <div class="d-flex">
+                                    <div>
+                                        <a href="" title="Vote Up" class="vote-up d-block text-center text-dark">
+                                            <i class="fa fa-caret-up fa-3x"></i>
+                                        </a>
+                                        <h4 class="m-0 text-muted">45</h4>
+                                        <a href="" title="Vote Down" class="vote-down d-block text-center text-dark">
+                                            <i class="fa fa-caret-down fa-3x"></i>
+                                        </a>
+                                    </div>
+                                    <div class="mt-3 ms-3">
+                                        <a href="" title="Mark as Fav" class="favorite d-block text-center mb-2">
+                                            <i class="fa fa-star fa-2x text-dark"></i>
+                                        </a>
+                                        <h4 class="text-muted">123</h4>
+                                    </div>
+                                </div>
                                 <div class="d-flex flex-column">
                                     <div class="text-end">
                                         Answered {{ $answer->created_date }}
