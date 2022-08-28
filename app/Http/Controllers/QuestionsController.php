c6408e93ab1820bcbd6460f192c282eff477d759
Kunal Nathani <kunalnathani2042@gmail.com>
2022-08-28 05:38:08 +0530

    feat: added form to edit question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -71,7 +71,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+        return view('questions.edit', compact(['question']));
     }
 
     /**
---
+++ resources/views/questions/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('frontend_layout.app')
+
+@section('page-level-styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.css" />
+@endsection
+
+@section('content')
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Ask a Question</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('questions.store') }}" method="POST">
+                            @csrf
+                            <div class="form-group mb-3">
+                                <label for="title">Title</label>
+                                <input type="text" placeholder="Enter Question Title" name="title" id="title"
+                                        class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}"
+                                        value="{{ old('title', $question->title) }}" />
+                                @error('title')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="body">Question</label>
+                                <input type="hidden" name="body" id="body"
+                                        value="{{ old('title', $question->title) }}"/>
+                                <trix-editor input="body" placeholder="Ask your question"
+                                             class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></trix-editor>
+                                @error('body')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <input type="submit" value="Update" class="btn btn-outline-primary">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('page-level-scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.3.1/trix.js"></script>
+@endsection
