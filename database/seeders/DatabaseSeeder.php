83ebb8c5e82c6d15a3c9c059ad0b9d51115a61ed
Kunal Nathani <kunalnathani2042@gmail.com>
2022-08-28 11:05:00 +0530

    added seeder for answers with questions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Answer;
 use App\Models\Question;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -19,10 +20,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        User::factory(10)->create()->each(function($user) {
-            for($i=1; $i<=random_int(2, 8); ++$i) {
-                $user->questions()->create(Question::factory()->make()->toArray());
-            }
+        User::factory(10)->create()->each(function ($user) {
+            $user->questions()
+                ->saveMany(Question::factory(random_int(2, 8))->make())
+                ->each(function ($question) {
+                    $question->answers()->saveMany(Answer::factory(random_int(5, 10))->make());
+                });
         });
     }
 }
