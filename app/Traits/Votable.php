7b6b15122e422e214cf83c10f6a795f5c1c2e030
Kunal Nathani <kunalnathani2042@gmail.com>
2022-09-04 17:48:21 +0530

    added methods to check and vote in trait

--- app/Traits/Votable.php
+++ app/Traits/Votable.php
@@ -2,6 +2,8 @@
 
 namespace App\Traits;
 
+use App\Models\User;
+
 trait Votable
 {
     public function vote(int $vote)
@@ -11,6 +13,33 @@ trait Votable
             $this->decrement('votes_count');
         } else {
             $this->increment('votes_count');
+        }
+    }
+
+    public function hasMarkedUpVote(User $user)
+    {
+        return $this->votes()->where('user_id', $user->id)->where('vote', 1)->exists();
+    }
+
+    public function hasMarkedDownVote(User $user)
+    {
+        return $this->votes()->where('user_id', $user->id)->where('vote', -1)->exists();
+    }
+
+    public function hasVoted(User $user)
+    {
+        return $this->votes()->where('user_id', $user->id)->exists();
+    }
+
+    public function updateVote(int $vote)
+    {
+        $this->votes()->updateExistingPivot(auth()->id(), ['vote'=> $vote]);
+        if($vote > 0) {
+            $this->increment('votes_count');
+            $this->increment('votes_count');
+        } else {
+            $this->decrement('votes_count');
+            $this->decrement('votes_count');
         }
     }
 }
